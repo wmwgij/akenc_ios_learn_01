3d8476defa9425ed7c6a816fc11d8fc980e15f7d
wmwgij <wmwgij@hotmail.com>
2015-09-28 11:16:50 +0800

    2.step2
    delete some file & change appDelegate :
    1.add self.window
    2.add self.viewController
    3.add window.rootViewController

--- akenc_ios_learn_01/akenc_ios_learn_01/AppDelegate.m
+++ akenc_ios_learn_01/akenc_ios_learn_01/AppDelegate.m
@@ -12,18 +12,29 @@
 
 @end
 
+
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    self.viewController = [[ViewController alloc] initWithNibName:nil bundle:nil];
+    self.window.rootViewController = self.viewController;
+    
+
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
+};
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
