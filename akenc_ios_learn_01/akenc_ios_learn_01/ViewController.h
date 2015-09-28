0e796a10a15841379ab26edab83488d9f91d93ec
wmwgij <wmwgij@hotmail.com>
2015-09-28 10:41:09 +0800

    first commit

---
+++ akenc_ios_learn_01/akenc_ios_learn_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  akenc_ios_learn_01
+//
+//  Created by kenshino on 15/9/28.
+//  Copyright © 2015年 akenc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ akenc_ios_learn_01/akenc_ios_learn_01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  akenc_ios_learn_01
+//
+//  Created by kenshino on 15/9/28.
+//  Copyright © 2015年 akenc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
